ec487f26f9ec4a128c06d67656a9616d4eb522dc
Gabrielly Oliveira <gabriellyorosimbooliveira@gmail.com>
2022-06-08 13:31:08 -0300

    Imprimir task 4

--- 0x00/salvando/sql_schema_first_db.sql
+++ 0x00/salvando/sql_schema_first_db.sql
@@ -1,6 +1,16 @@
+-- TABLE
+
 CREATE TABLE cliente(
 id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
 nome VARCHAR(80) NOT NULL,
 email VARCHAR(40) NOT NULL,
 telefone INTEGER
-)
+);
+
+CREATE TABLE sqlite_sequence(name,seq);
+
+-- INDEX
+
+-- TRIGGER
+
+-- VIEW
