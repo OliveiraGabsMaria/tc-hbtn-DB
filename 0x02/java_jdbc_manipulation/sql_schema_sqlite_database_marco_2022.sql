28698db7ba7d5f951451c153db160bf449a773e0
Gabrielly Oliveira <gabriellyorosimbooliveira@gmail.com>
2022-06-13 12:54:10 -0300

    Imprimir task 2

---
+++ 0x02/java_jdbc_manipulation/sql_schema_sqlite_database_marco_2022.sql
@@ -0,0 +1,15 @@
+-- TABLE
+CREATE TABLE Cliente(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  nome VARCHAR(255) NOT NULL,
+  idade INTEGER NOT NULL,
+  cpf VARCHAR(14) NOT NULL,
+  rg VARCHAR(14) NOT NULL);
+CREATE TABLE sqlite_sequence(name,seq);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
