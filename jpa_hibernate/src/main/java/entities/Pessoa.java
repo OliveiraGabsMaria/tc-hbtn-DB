2553b1ddbfb508194ba6412f60526f973ffdc65c
Gabrielly Oliveira <gabriellyorosimbooliveira@gmail.com>
2022-06-17 11:02:52 -0300

    Imprimir task 0

---
+++ jpa_hibernate/src/main/java/entities/Pessoa.java
@@ -0,0 +1,73 @@
+package entities;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "pessoa" ,
+uniqueConstraints = {@UniqueConstraint(columnNames = {"cpf"})}
+)
+public class Pessoa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column
+    private String nome;
+    @Column
+    private String email;
+    @Column
+    private int idade;
+    @Column
+    private String cpf;
+    @Column
+    private LocalDate dataDeNascimento;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public LocalDate getDataDeNascimento() {
+        return dataDeNascimento;
+    }
+
+    public void setDataDeNascimento(LocalDate dataDeNascimento) {
+        this.dataDeNascimento = dataDeNascimento;
+    }
+}
